71f139aff90c6119efa03e81795b3516217e6305
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-11-30 16:46:13 +0900

    컨텍스트 스위칭 간단 테스트

---
+++ src/main/java/chaptor01/exam02/ContextSwitchExample.java
@@ -0,0 +1,44 @@
+package chaptor01.exam02;
+
+public class ContextSwitchExample {
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Thread 1: " + i);
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Thread 2: " + i);
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Thread thread3 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Thread 3: " + i);
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        // 총 3개의 쓰레드를 실행하면 컨텍스트 스위칭이 일어나면서 번갈아가며 콘솔에 출력된다.
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
